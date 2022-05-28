7c1b14220ee64eca87a8cf5fed96697aea875ba6
Adam <adammoore118@outlook.com>
2022-05-28 15:56:48 -0500

    content ideas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 export default function () {
     return (
-        <footer class="text-center lg:text-left bg-blue-800 text-gray-300">
-            <div class="flex justify-center items-center p-6 border-b-4 border-yellow-300 grid-2">
+        <footer className="text-center lg:text-left bg-blue-800 text-gray-300">
+            <div className="flex justify-center items-center p-6 border-b-4 border-yellow-300 grid-2">
 
                 <div className='mr-12 text-white'>
                     <em>"COMMUNITY IS THE FOUNDATION TO SUCCESS"</em>
                 </div>
 
-                <div class="flex justify-center items-center ">
+                <div className="flex justify-center items-center ">
 
-                    <a href="#!" class="text-red-600 ml-2 mr-2">
+                    <a href="#!" className="text-red-600 ml-2 mr-2">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f"
-                            class="w-2.5" role="img" xmlns="http://www.w3.org/2000/svg"
+                            className="w-2.5" role="img" xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 320 512">
                             <path fill="currentColor"
                                 d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
@@ -22,16 +22,16 @@ export default function () {
 
                     </a>
 
-                    <a href="#!" class="text-white ml-2 mr-2">
+                    <a href="#!" className="text-white ml-2 mr-2">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube"
-                            class="w-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                            className="w-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                             <path fill="currentColor"
                                 d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
                         </svg>
                     </a >
-                    <a href="#!" class="text-blue-600 ml-2 mr-2">
+                    <a href="#!" className="text-blue-600 ml-2 mr-2">
                         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="website"
-                            class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            className="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path fill="currentColor"
                                 d="M352 256C352 278.2 350.8 299.6 348.7 320H163.3C161.2 299.6 159.1 278.2 159.1 256C159.1 233.8 161.2 212.4 163.3 192H348.7C350.8 212.4 352 233.8 352 256zM503.9 192C509.2 212.5 512 233.9 512 256C512 278.1 509.2 299.5 503.9 320H380.8C382.9 299.4 384 277.1 384 256C384 234 382.9 212.6 380.8 192H503.9zM493.4 160H376.7C366.7 96.14 346.9 42.62 321.4 8.442C399.8 29.09 463.4 85.94 493.4 160zM344.3 160H167.7C173.8 123.6 183.2 91.38 194.7 65.35C205.2 41.74 216.9 24.61 228.2 13.81C239.4 3.178 248.7 0 256 0C263.3 0 272.6 3.178 283.8 13.81C295.1 24.61 306.8 41.74 317.3 65.35C328.8 91.38 338.2 123.6 344.3 160H344.3zM18.61 160C48.59 85.94 112.2 29.09 190.6 8.442C165.1 42.62 145.3 96.14 135.3 160H18.61zM131.2 192C129.1 212.6 127.1 234 127.1 256C127.1 277.1 129.1 299.4 131.2 320H8.065C2.8 299.5 0 278.1 0 256C0 233.9 2.8 212.5 8.065 192H131.2zM194.7 446.6C183.2 420.6 173.8 388.4 167.7 352H344.3C338.2 388.4 328.8 420.6 317.3 446.6C306.8 470.3 295.1 487.4 283.8 498.2C272.6 508.8 263.3 512 255.1 512C248.7 512 239.4 508.8 228.2 498.2C216.9 487.4 205.2 470.3 194.7 446.6H194.7zM190.6 503.6C112.2 482.9 48.59 426.1 18.61 352H135.3C145.3 415.9 165.1 469.4 190.6 503.6V503.6zM321.4 503.6C346.9 469.4 366.7 415.9 376.7 352H493.4C463.4 426.1 399.8 482.9 321.4 503.6V503.6z" />
                         </svg>
@@ -39,14 +39,14 @@ export default function () {
 
                 </div>
             </div>
-            <div class="mx-6 py-10 text-center text-white md:text-left">
-                <div class="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                    <div class="">
+            <div className="mx-6 py-10 text-center text-white md:text-left">
+                <div className="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                    <div className="">
                  
                      
                     
                   
-                        <h6 class="
+                        <h6 className="
             uppercase
             
             mb-4
@@ -63,87 +63,87 @@ export default function () {
                       <p>
                      
                       
-                      <i class="fa-solid fa-fire-hydrant text-2xl text-red-700 mr-2 ml-2"></i>
-                      <i class="fa-solid fa-fire-extinguisher text-2xl text-red-700 mr-2 ml-2"></i>
-                      <i class="fa-solid fa-fire-smoke text-2xl text-red-700 mr-2 ml-2"></i>
-                      <i class="fa-solid fa-hose-reel text-2xl text-red-700 mr-2 ml-2"></i>
+                      <i className="fa-solid fa-fire-hydrant text-2xl text-red-700 mr-2 ml-2"></i>
+                      <i className="fa-solid fa-fire-extinguisher text-2xl text-red-700 mr-2 ml-2"></i>
+                      <i className="fa-solid fa-fire-smoke text-2xl text-red-700 mr-2 ml-2"></i>
+                      <i className="fa-solid fa-hose-reel text-2xl text-red-700 mr-2 ml-2"></i>
                       </p>
                       
                      
                      
                     </div>
-                    <div class="">
-                        <h6 class="uppercase mb-4 flex justify-center md:justify-start text-yellow-300 underline">
+                    <div className="">
+                        <h6 className="uppercase mb-4 flex justify-center md:justify-start text-yellow-300 underline">
                             Internal Training Information
                         </h6>
-                        <p class="mb-4">
-                            <a href="#!" class="text-yellow-300 no-underline hover:text-red-700">Personnel Promotional Roadmaps</a>
+                        <p className="mb-4">
+                            <a href="#!" className="text-yellow-300 no-underline hover:text-red-700">Personnel Promotional Roadmaps</a>
                         </p>
-                        <p class="mb-4">
-                            <a href="#!" class="text-yellow-300 no-underline hover:text-red-700">Department Policies</a>
+                        <p className="mb-4">
+                            <a href="#!" className="text-yellow-300 no-underline hover:text-red-700">Department Policies</a>
                         </p>
-                        <p class="mb-4">
-                            <a href="#!" class="text-yellow-300 no-underline hover:text-red-700">FAQs</a>
+                        <p className="mb-4">
+                            <a href="#!" className="text-yellow-300 no-underline hover:text-red-700">FAQs</a>
                         </p>
                         <p>
-                            <a href="#!" class="text-yellow-300 no-underline hover:text-red-700">Public Information</a>
+                            <a href="#!" className="text-yellow-300 no-underline hover:text-red-700">Public Information</a>
                         </p>
                     </div>
-                    <div class="">
-                        <h6 class="uppercase mb-4 flex justify-center md:justify-start text-yellow-300 underline">
+                    <div className="">
+                        <h6 className="uppercase mb-4 flex justify-center md:justify-start text-yellow-300 underline">
                             External Training Information
                         </h6>
-                        <p class="mb-4">
-                            <a href="#!" class="text-yellow-300 no-underline hover:text-red-700">Mississippi State Fire Academy</a>
+                        <p className="mb-4">
+                            <a href="#!" className="text-yellow-300 no-underline hover:text-red-700">Mississippi State Fire Academy</a>
                         </p>
-                        <p class="mb-4">
-                            <a href="#!" class="text-yellow-300 no-underline hover:text-red-700">USFA (NFA)</a>
+                        <p className="mb-4">
+                            <a href="#!" className="text-yellow-300 no-underline hover:text-red-700">USFA (NFA)</a>
                         </p>
-                        <p class="mb-4">
-                            <a href="#!" class="text-yellow-300 no-underline hover:text-red-700">NFPA</a>
+                        <p className="mb-4">
+                            <a href="#!" className="text-yellow-300 no-underline hover:text-red-700">NFPA</a>
                         </p>
                         <p>
-                            <a href="#!" class="text-yellow-300 no-underline hover:text-red-700">IFSAC</a>
+                            <a href="#!" className="text-yellow-300 no-underline hover:text-red-700">IFSAC</a>
                         </p>
                     </div>
-                    <div class="">
-                        <h6 class="uppercase underline mb-4 flex justify-center md:justify-start text-yellow-300">
+                    <div className="">
+                        <h6 className="uppercase underline mb-4 flex justify-center md:justify-start text-yellow-300">
                             Training Chief Eric Valles
                         </h6>
-                        <p class="flex items-center justify-center md:justify-start mb-4 text-yellow-300">
+                        <p className="flex items-center justify-center md:justify-start mb-4 text-yellow-300">
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home"
-                                class="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                className="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                                 <path fill="currentColor"
                                     d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z">
                                 </path>
                             </svg>
                             1515 23rd Ave, Gulfport, MS 39501
                         </p>
-                        <p class="flex items-center justify-center md:justify-start mb-4 text-yellow-300">
+                        <p className="flex items-center justify-center md:justify-start mb-4 text-yellow-300">
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope"
-                                class="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg"
+                                className="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 512 512">
                                 <path fill="currentColor"
                                     d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z">
                                 </path>
                             </svg>
-                            <a className='text-yellow-300 no-underline' href="mailto:evalles@gulfport-ms.gov">evalles@gulfport-ms.gov</a>
+                            <a className='text-yellow-300 no-underline hover:text-red-700' href="mailto:evalles@gulfport-ms.gov">evalles@gulfport-ms.gov</a>
                         </p>
-                        <p class="flex items-center justify-center md:justify-start mb-4 text-yellow-300">
+                        <p className="flex items-center justify-center md:justify-start mb-4 text-yellow-300">
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone"
-                                class="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg"
+                                className="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 512 512">
                                 <path fill="currentColor"
                                     d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z">
                                 </path>
                             </svg>
-                            <a className='text-yellow-300 no-underline' href="tel:,">(228) 860-5950 ext. XXXX</a>
+                            <a className='text-yellow-300 no-underline hover:text-red-700' href="tel:,">(228) 860-5950 ext. XXXX</a>
                         </p>
                     
                     </div>
                 </div>
             </div>
-            <div class="text-center text-sm p-2 border-t-4 border-yellow-300 ">
+            <div className="text-center text-sm p-2 border-t-4 border-yellow-300 ">
                 <span>© 2022 Gulfport Fire Department Administration and Personnel</span>
                 
             </div>
