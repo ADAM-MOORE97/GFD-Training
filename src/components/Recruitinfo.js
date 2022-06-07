57019480bf44c8dd333679139e698901e7dc922e
Adam <adammoore118@outlook.com>
2022-06-07 15:07:23 -0500

    styles

--- src/components/Recruitinfo.js
+++ src/components/Recruitinfo.js
@@ -10,7 +10,7 @@ export default function Recruitinfo() {
   const roster = ['John Smith #001', 'John Smith #001', 'John Smith #001', 'John Smith #001', 'John Smith #001', 'John Smith #001', 'John Smith #001', 'John Smith #001', 'John Smith #001', 'John Smith #001']
   return (
     <div className='border-2 border-solid bg-slate-50 mx-auto mt-6 mb-6 p-10 md:w-3/4'>
-      <h3 className='mt-6 mb-6 text-xl md:text-2xl lg:text-3xl xl:text-4xl'>Recruit Class 2202</h3>
+      <h3 className='mt-6 mb-6 text-xl bg-slate-300 md:text-2xl lg:text-3xl xl:text-4xl'>Recruit Class 2202</h3>
       <Swiper
         className='h-96'
         modules={[Navigation, Pagination, Scrollbar]}
@@ -30,14 +30,14 @@ export default function Recruitinfo() {
       </Swiper>
 
 
-      <div className='mx-auto bg-red-600 text-white mt-6 p-6'>
+      <div className='mx-auto rounded-2xl bg-red-600 text-white mt-6 p-6'>
         <h4 className='underline'>Class Roster</h4>
-        {roster.map(names => <em className='mr-3 ml-3'>{names}</em>)}
+        {roster.map(names => <em className='flex justify-center mr-3 ml-3'>{names}</em>)}
         <div className='mt-6'>
-          <em> "Insert recruit class motivational statement here" -Class 2202</em>
+          <em> "Insert recruit class motivational statement here" <p>-Class 2202</p></em>
         </div>
       </div>
-  <div className='mt-6 mx-auto bg-blue-800 rounded-xl w-1/4'>
+  <div className='mt-6 mx-auto bg-blue-800 rounded-xl md:w-1/4'>
 <p>
 <a href='#' className='text-xl text-slate-50 no-underline hover:text-yellow-300'>
       
